7fb4dc23c586c6be8f9b07cbf759edab3ab40250
qwe3804132 <liupeiran9324@outlook.com>
2019-01-12 23:01:51 -0500

    First commit

---
+++ Scripts/app.js
@@ -0,0 +1,26 @@
+//"use strict"
+
+(function(){      //auto excuate function because it is anonymous Function
+    //IIFE   Immediately Invoked Function Expression
+let myFunctionVariable = 0;
+
+
+let Start=function(){//better way to do in this way
+
+    let startVariable = 0;
+    let myFunctionVariable = 5;
+   // myFunctionVariable="Tom";   //this is not healthy
+    window.console.log(`%cApp Started...${myFunctionVariable}`,"font-size:20px;color:blue",);//or use console.info
+}
+window.addEventListener("load",Start);
+
+
+
+})();
+
+
+
+
+
+
+//window.addEventListener("load",())
